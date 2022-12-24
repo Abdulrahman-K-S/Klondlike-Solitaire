4d5be796cd1ea339483d561e375f9b15c93c52ad
Abdurlahman Khaled <107813045+Bebo-K-S@users.noreply.github.com>
2022-12-24 14:40:30 +0200

    Update Card.cpp

--- src/Card.cpp
+++ src/Card.cpp
@@ -3,23 +3,23 @@
 void Deck::setDeck() const
 {
     int index = 0;
-    for(int suit = (int)Suits::CLUBS; suit <= (int)Suits::SPADES; ++suit)
+    for (int suit = (int)Suits::CLUBS; suit <= (int)Suits::SPADES; ++suit)
     {
-        for(int name = (int)CardNames::ACE; name <= (int)CardNames::KING; ++name)
+        for (int name = (int)CardNames::ACE; name <= (int)CardNames::KING; ++name)
         {
             Card card;
-            if(suit == (int)Suits::CLUBS || suit == (int)Suits::SPADES)
+            if (suit == (int)Suits::CLUBS || suit == (int)Suits::SPADES)
             {
                 card.color = CardColor::BLACK;
-            } 
-            else 
+            }
+            else
             {
                 card.color = CardColor::RED;
             }
             card.name = (CardNames)name;
             card.suit = (Suits)suit;
             card.is_flipped = false;
-            card.img_file.loadFromFile("cards/" + convert_suits[suit - 1] + " - "+ convert_names[name - 1] + ".png");
+            card.img_file.loadFromFile("Assets/cards/" + convert_suits[suit - 1] + " - " + convert_names[name - 1] + ".png");
             cards[index] = card;
             ++index;
         }
@@ -28,7 +28,7 @@ void Deck::setDeck() const
 
 void Deck::shuffleCards() const
 {
-    srand (time(nullptr));
+    srand(time(nullptr));
     for (int i = 51; i > 0; i--)
     {
         int j = rand() % (i + 1);
@@ -36,7 +36,7 @@ void Deck::shuffleCards() const
     }
 }
 
-void Deck::swap(Card *a, Card *b)
+void Deck::swap(Card* a, Card* b)
 {
     Card temp = *a;
     *a = *b;
