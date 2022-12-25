7bb9dfd87f04eba0d11a4d9f0d28fe0afb7742ca
OmarElabasery <110893051+OmarElabasery@users.noreply.github.com>
2022-12-25 14:13:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 // Including the different classes
-#include "src/OutlinePile.h"
-#include "src/Pile.h"
+#include "OutlinePile.h"
+#include "Pile.h"
 
 // Using Namespace
 using namespace sf;
@@ -13,12 +13,12 @@ using namespace std;
 // Global Variables:
 
 // The 12 outline piles
-OutlinePile pilesArr[12] = {OutlinePile(290,120),OutlinePile(740,120), OutlinePile(890,120),
+OutlinePile pilesArr[12] = { OutlinePile(290,120),OutlinePile(740,120), OutlinePile(890,120),
                             OutlinePile(1040,120), OutlinePile(1190,120), OutlinePile(290, 370),
                             OutlinePile(440, 370), OutlinePile(590, 370), OutlinePile(740, 370),
-                            OutlinePile(890,370), OutlinePile(1040,370), OutlinePile(1190,370)};
+                            OutlinePile(890,370), OutlinePile(1040,370), OutlinePile(1190,370) };
 
-void DrawPiles(RenderWindow &window)
+void DrawPiles(RenderWindow& window)
 {
     for (int i = 0; i < 12; i++)
         window.draw(pilesArr[i].getShape());
@@ -34,7 +34,7 @@ int main()
     Sprite backgroundSpr(backgroundTex);
 
     // Setting up the Scoreboard
-    RectangleShape scoreBoard(Vector2f(1600,80));
+    RectangleShape scoreBoard(Vector2f(1600, 80));
     scoreBoard.setFillColor(Color(169, 169, 169, 128));
 
     //Setting the font and adding the score text
@@ -44,7 +44,15 @@ int main()
     score.setFont(scoreFont);
     score.setString("Score: 00");
     score.setCharacterSize(20);
-    score.setPosition(730, 30);
+    score.setPosition(600, 30);
+
+    Text Timer;
+    Clock clock;
+    Time timeElapsed1 = clock.getElapsedTime();
+    Timer.setFont(scoreFont);
+    Timer.setString("Timer: " + to_string(timeElapsed1.asSeconds()));
+    Timer.setCharacterSize(20);
+    Timer.setPosition(800, 30);
 
     Pile pile;
     pile.setDeck();
@@ -68,8 +76,21 @@ int main()
         window.draw(backgroundSpr);
         window.draw(scoreBoard);
         DrawPiles(window);
+        Time timeElapsed1 = clock.getElapsedTime();
+        int Minutes = int(timeElapsed1.asSeconds())/60;
+        int Seconds = int(timeElapsed1.asSeconds())%60;
+        if(Minutes < 10 && Seconds >= 10)
+            Timer.setString("Timer: 0" + to_string(Minutes) + ":" + to_string(Seconds));
+        else if(Seconds < 10 && Minutes >= 10)
+            Timer.setString("Timer: " + to_string(Minutes) + ":0" + to_string(Seconds));
+        else if(Minutes < 10 && Seconds < 10)
+            Timer.setString("Timer: 0" + to_string(Minutes) + ":0" + to_string(Seconds));
+        else
+            Timer.setString("Timer: " + to_string(Minutes) + ":" + to_string(Seconds));
+        window.draw(Timer);
         window.draw(score);
-        for(int i = 0; i < 52; ++i){
+        window.draw(Timer);
+        for (int i = 0; i < 52; ++i) {
             window.draw(pile.cards[i].card_sprite);
         }
         window.display();
