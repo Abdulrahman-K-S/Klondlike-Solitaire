bf4f14f0ef852278014b9a36b04d1eefe9028aff
Abdurlahman Khaled <107813045+Bebo-K-S@users.noreply.github.com>
2022-12-25 23:37:26 +0200

    Create Game.cpp

---
+++ src/Game.cpp
@@ -0,0 +1,10 @@
+// Including the sfml and iostream libraires
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// Including the outline class
+#include "OutlinePile.h"
+
+// Using Namespace
+using namespace sf;
+using namespace std;
