d85c2c9652777ae53be584d610e64eff9b03512b
OmarElabasery <110893051+OmarElabasery@users.noreply.github.com>
2022-12-26 15:23:45 +0200

    Update Pile.cpp

--- src/Pile.cpp
+++ src/Pile.cpp
@@ -89,42 +89,42 @@ void Pile::setPileCards()
 {
     for (int i = 0; i < 52; i++)
     {
-        if (i == 0) 
+        if (i == 0)
         {
             cards[i].pile = CardPile::NORMAL_PILE1;
             setXYCoordinates(cards[i]);
         }
-        else if (i >= 1 && i <= 2) 
+        else if (i >= 1 && i <= 2)
         {
             cards[i].pile = CardPile::NORMAL_PILE2;
             setXYCoordinates(cards[i]);
         }
-        else if (i >= 3 && i <= 5) 
+        else if (i >= 3 && i <= 5)
         {
             cards[i].pile = CardPile::NORMAL_PILE3;
             setXYCoordinates(cards[i]);
         }
-        else if (i >= 6 && i <= 9) 
+        else if (i >= 6 && i <= 9)
         {
             cards[i].pile = CardPile::NORMAL_PILE4;
             setXYCoordinates(cards[i]);
         }
-        else if (i >= 10 && i <= 14) 
+        else if (i >= 10 && i <= 14)
         {
             cards[i].pile = CardPile::NORMAL_PILE5;
             setXYCoordinates(cards[i]);
         }
-        else if (i >= 15 && i <= 20) 
+        else if (i >= 15 && i <= 20)
         {
             cards[i].pile = CardPile::NORMAL_PILE6;
             setXYCoordinates(cards[i]);
         }
-        else if (i >= 21 && i <= 27) 
+        else if (i >= 21 && i <= 27)
         {
             cards[i].pile = CardPile::NORMAL_PILE7;
             setXYCoordinates(cards[i]);
         }
-        else 
+        else
         {
             cards[i].pile = CardPile::SHUFFLED_PILE;
             draw_pile.push(cards[i]);
@@ -139,83 +139,78 @@ void Pile::setSpriteTexture()
     {
         if (i == 0 || i == 1 || i == 3 || i == 6 || i == 10 || i == 15 || i == 21)
         {
+            cards[i].card_sprite.setTexture(cards[i].back_img_file);
+            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate);
+
             if (i == 0)
             {
                 cards[i].card_sprite.setTexture(cards[i].front_img_file);
                 cards[i].is_flipped = true;
             }
-
-            cards[i].card_sprite.setTexture(cards[i].back_img_file);
-            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate);
         }
         else if (i == 2 || i == 4 || i == 7 || i == 11 || i == 16 || i == 22)
         {
+            cards[i].card_sprite.setTexture(cards[i].back_img_file);
+            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 20);
+            cards[i].yCoordinate = cards[i].yCoordinate + 20;
+
             if (i == 2)
             {
                 cards[i].card_sprite.setTexture(cards[i].front_img_file);
                 cards[i].is_flipped = true;
-                cards[i].yCoordinate = cards[i].yCoordinate + 20;
             }
-
-            cards[i].card_sprite.setTexture(cards[i].back_img_file);
-            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 20);
-            cards[i].yCoordinate = cards[i].yCoordinate + 20;
         }
         else if (i == 5 || i == 8 || i == 12 || i == 17 || i == 23)
         {
+            cards[i].card_sprite.setTexture(cards[i].back_img_file);
+            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 40);
+            cards[i].yCoordinate = cards[i].yCoordinate + 40;
+
             if (i == 5)
             {
                 cards[i].card_sprite.setTexture(cards[i].front_img_file);
                 cards[i].is_flipped = true;
-                cards[i].yCoordinate = cards[i].yCoordinate + 40;
             }
-
-            cards[i].card_sprite.setTexture(cards[i].back_img_file);
-            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 40);
-            cards[i].yCoordinate = cards[i].yCoordinate + 40;
         }
         else if (i == 9 || i == 13 || i == 18 || i == 24)
         {
+            cards[i].card_sprite.setTexture(cards[i].back_img_file);
+            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 60);
+            cards[i].yCoordinate = cards[i].yCoordinate + 60;
+
             if (i == 9)
             {
                 cards[i].card_sprite.setTexture(cards[i].front_img_file);
                 cards[i].is_flipped = true;
-                cards[i].yCoordinate = cards[i].yCoordinate + 60;
             }
-
-            cards[i].card_sprite.setTexture(cards[i].back_img_file);
-            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 60);
-            cards[i].yCoordinate = cards[i].yCoordinate + 60;
         }
         else if (i == 14 || i == 19 || i == 25)
         {
+            cards[i].card_sprite.setTexture(cards[i].back_img_file);
+            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 80);
+            cards[i].yCoordinate = cards[i].yCoordinate + 80;
+
             if (i == 14)
             {
                 cards[i].card_sprite.setTexture(cards[i].front_img_file);
                 cards[i].is_flipped = true;
-                cards[i].yCoordinate = cards[i].yCoordinate + 80;
             }
-
-            cards[i].card_sprite.setTexture(cards[i].back_img_file);
-            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 80);
-            cards[i].yCoordinate = cards[i].yCoordinate + 80;
         }
         else if (i == 20 || i == 26)
         {
+            cards[i].card_sprite.setTexture(cards[i].back_img_file);
+            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 100);
+            cards[i].yCoordinate = cards[i].yCoordinate + 100;
+
             if (i == 20)
             {
                 cards[i].card_sprite.setTexture(cards[i].front_img_file);
                 cards[i].is_flipped = true;
-                cards[i].yCoordinate = cards[i].yCoordinate + 100;
             }
-
-            cards[i].card_sprite.setTexture(cards[i].back_img_file);
-            cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 100);
-            cards[i].yCoordinate = cards[i].yCoordinate + 100;
         }
         else if (i == 27)
-        {            
-            cards[i].card_sprite.setTexture(cards[i].back_img_file);
+        {
+            cards[i].card_sprite.setTexture(cards[i].front_img_file);
             cards[i].card_sprite.setPosition(cards[i].xCoordinate, cards[i].yCoordinate + 120);
             cards[i].yCoordinate = cards[i].yCoordinate + 120;
         }
