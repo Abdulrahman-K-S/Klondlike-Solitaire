7f281f098244e43607c35682c8f98749053fdc18
Abdurlahman Khaled <107813045+Bebo-K-S@users.noreply.github.com>
2022-12-27 14:41:05 +0200

    Update Pile.h

--- src/Pile.h
+++ src/Pile.h
@@ -10,25 +10,42 @@ using namespace std;
 class Pile
 {
 public:
-    Card* card_sprites = new Card[52];
     Card* normal_pile1 = new Card[13];
+    int normal_pile1_index = 0;
+
     Card* normal_pile2 = new Card[14];
+    int normal_pile2_index = 0;
+
     Card* normal_pile3 = new Card[15];
+    int normal_pile3_index = 0;
+
     Card* normal_pile4 = new Card[16];
+    int normal_pile4_index = 0;
+
     Card* normal_pile5 = new Card[17];
+    int normal_pile5_index = 0;
+
     Card* normal_pile6 = new Card[18];
+    int normal_pile6_index = 0;
+
     Card* normal_pile7 = new Card[19];
+    int normal_pile7_index = 0;
+
     stack<Card> foundation_pile1;
     stack<Card> foundation_pile2;
     stack<Card> foundation_pile3;
     stack<Card> foundation_pile4;
     stack<Card> shuffled_pile;
     stack<Card> draw_pile;
-    Card cards[52];
+
+    Card* cards = new Card[52];
+    Pile();
     void setDeck();
     void shuffleCards();
-    static void swap(Card*, Card*);
+    void swap(Card*, Card*);
     void setSpriteTexture();
     void setPileCards();
     void setXYCoordinates(Card&);
+
+    void displayCards(RenderWindow& window);
 };
