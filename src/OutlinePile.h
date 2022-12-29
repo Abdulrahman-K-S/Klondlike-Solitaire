f78ce0602c35590547ecaa647dab42121ee7ea41
Abdurlahman Khaled <107813045+Bebo-K-S@users.noreply.github.com>
2022-12-29 23:42:23 +0200

    Update OutlinePile.h

--- src/OutlinePile.h
+++ src/OutlinePile.h
@@ -6,10 +6,10 @@ using namespace std;
 
 class OutlinePile
 {
-private:
-    RectangleShape pileOutline; // Defining the outline shape for the pile
-    int xPosition, yPosition; // Having the position known for later use
 public:
-    OutlinePile(int xPosition, int yPosition);
-    RectangleShape getShape();
+    RectangleShape pileOutline;
+    int startXPosition = 0, startYPosition = 0;
+    int endXPosition = 0, endYPosition = 0;
+    OutlinePile(int startXPosition, int startYPosition,\
+        int endXposition, int endYposition);
 };
