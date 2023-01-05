7564280913dfee88dc23f05e20205c6d32f4ff1e
Abdurlahman Khaled <107813045+Bebo-K-S@users.noreply.github.com>
2023-01-05 04:16:01 +0200

    Update OutlinePile.h

--- src/OutlinePile.h
+++ src/OutlinePile.h
@@ -1,15 +1,10 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
-
 using namespace sf;
-using namespace std;
 
 class OutlinePile
 {
 public:
     RectangleShape pileOutline;
-    int startXPosition = 0, startYPosition = 0;
-    int endXPosition = 0, endYPosition = 0;
-    OutlinePile(int startXPosition, int startYPosition,\
-        int endXposition, int endYposition);
+    OutlinePile();
+    OutlinePile(int, int);
 };
