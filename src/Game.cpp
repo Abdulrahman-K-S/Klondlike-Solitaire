7405b33a55883e616e1b6528aa0dd2cd831d1c91
Abdurlahman Khaled <107813045+Bebo-K-S@users.noreply.github.com>
2023-01-05 04:14:59 +0200

    Update Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,154 @@
 // Including the sfml and iostream libraires
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
-// Including the outline class
-#include "OutlinePile.h"
+#include "Game.h"
 
 // Using Namespace
 using namespace sf;
 using namespace std;
+
+Game::Game()
+{
+	// Using all the functions once the object is created to save time
+	setMainMenuText();
+	setMainMenuMusic();
+	setMainMenuButton();
+    
+	setBackground();
+    setScoreboard();
+	setOutlinePiles();
+	setMenu();
+    
+	setFont();
+    setTimer();
+}
+
+void Game::setBackground()
+{
+    // The function sets the background texture and set the sprite
+    // with that texture
+    background_texture.loadFromFile("Assets/Background.jpg");
+    background.setTexture(background_texture);
+}
+
+void Game::setScoreboard()
+{
+	// The function sets the scoreboard's size and it's fill
+	// color
+    score_board.setSize(Vector2f(1600, 80));
+    score_board.setFillColor(Color(169, 169, 169, 128));
+	score.setString("Score: 00");
+	score.setCharacterSize(20);
+	score.setPosition(600, 30);
+}
+
+void Game::setMenu()
+{
+	// The function sets the menu box and it's content (resume button &
+	// quit button)
+    menu_box.setSize(Vector2f(400, 500));
+    menu_box.setPosition(600, 200);
+    menu_box.setFillColor(Color(113, 147, 128, 245));
+    
+    resume.setSize(Vector2f(300, 150));
+    resume.setPosition(650, 335);
+    resume.setFillColor(Color(94, 193, 140));
+    
+    quit.setSize(Vector2f(300, 150));
+    quit.setPosition(650, 520);
+    quit.setFillColor(Color(94, 193, 140));
+}
+
+void Game::setFont()
+{
+	// The function sets the font for the game
+	font.loadFromFile("Assets/Martian_Mono/MartianMono-VariableFont_wdth,wght.ttf");
+	score.setFont(font);
+}
+
+void Game::setTimer()
+{
+	// The function sets the timer for the game
+    timer.setFont(font);
+    timer.setCharacterSize(20);
+    timer.setPosition(800, 30);
+}
+
+void Game::setMainMenuMusic()
+{
+	// The function sets the main menu music
+	window_start_buffer.loadFromFile("Assets/Sounds/Start Sound.wav");
+	window_start.setBuffer(window_start_buffer);
+	music_start_buffer.loadFromFile("Assets/Sounds/Start Music.wav");
+	music_start.setBuffer(music_start_buffer);
+}
+
+void Game::setMainMenuText()
+{
+	// The function sets the main menu text which includes the "Made By Tryerz" text and image of the team
+	made_by.setFont(font);
+	made_by.setString("Made  By\n TRYERZ");
+	made_by.setCharacterSize(20);
+	made_by.setPosition(1300, 680);
+	tryerz_texture.loadFromFile("Assets/Tryerz.jpg");
+	tryerz_image.setTexture(tryerz_texture);
+	tryerz_image.setPosition(1290, 740);
+	tryerz_image.setScale(0.23, 0.23);
+	
+	// The "Start Game" blue button is set with the "Welcome to the Tryerz Solitare" text
+	start.setFont(font);
+	start.setString("Start Game");
+	start.setCharacterSize(25);
+	start.setPosition(700, 435);
+	start_message.setFont(font);
+	start_message.setString("Welcome to the Tryerz Solitaire!!\n\n\n\n\n\n\n");
+	start_message.setCharacterSize(25);
+	start_message.setPosition(490, 300);
+}
+
+void Game::setMainMenuButton()
+{
+	// The function sets the start button's (Size, Position & Color)
+	start_button.setSize(Vector2f(250, 70));
+	start_button.setPosition(Vector2f(675, 415));
+	start_button.setFillColor(Color(0, 181, 212, 700));
+}
+
+void Game::setEndMenu()
+{
+	// The function sets the background blurred image and 
+	// the "You Won!" text
+	blurred_page.setSize(Vector2f(1600, 900));
+	blurred_page.setFillColor(Color(255, 255, 255, 75));
+
+	you_won.setFont(font);
+	you_won.setString("You Won!!");
+	you_won.setCharacterSize(50);
+	you_won.setPosition(Vector2f(655, 300));
+	you_won.setFillColor(Color::Black);
+}
+
+void Game::setOutlinePiles()
+{	
+	// The function sets all 12 outline piles so we can draw them
+	// through out the game
+	outline_piles = new OutlinePile[12];
+	outline_piles[0] = OutlinePile(290, 120);
+	outline_piles[1] = OutlinePile(740, 120);
+	outline_piles[2] = OutlinePile(890, 120);
+	outline_piles[3] = OutlinePile(1040, 120);
+	outline_piles[4] = OutlinePile(1190, 120);
+	outline_piles[5] = OutlinePile(290, 370);
+	outline_piles[6] = OutlinePile(440, 370);
+	outline_piles[7] = OutlinePile(590, 370);
+	outline_piles[8] = OutlinePile(740, 370);
+	outline_piles[9] = OutlinePile(890,370);
+	outline_piles[10] = OutlinePile(1040,370);
+	outline_piles[11] = OutlinePile(1190, 370);
+}
+
+void Game::drawOutlinePiles(RenderWindow& window)
+{
+	for (int i = 0; i < 12; i++)
+		window.draw(outline_piles[i].pileOutline);
+}
