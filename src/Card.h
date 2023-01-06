77236d9f9bef9f76406ab32eeb152949b951efbc
Abdurlahman Khaled <107813045+Bebo-K-S@users.noreply.github.com>
2023-01-06 21:36:18 +0200

    Update Card.h

--- src/Card.h
+++ src/Card.h
@@ -1,34 +1,98 @@
-#include <SFML/Audio.hpp>
-#include "OutlinePile.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <stack>
+#include <random>
+#define stringify( name ) #name
 
-class Game
+using namespace sf;
+using namespace std;
+
+enum class Suits
+{
+    CLUBS = 1,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+};
+enum class CardNames
+{
+    ACE = 1,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+};
+
+enum class CardColor
+{
+    RED,
+    BLACK
+};
+
+enum class CardPile 
+{
+    SHUFFLED_PILE,
+    DRAW_PILE,
+    NORMAL_PILE1,
+    NORMAL_PILE2,
+    NORMAL_PILE3,
+    NORMAL_PILE4,
+    NORMAL_PILE5,
+    NORMAL_PILE6,
+    NORMAL_PILE7,
+    FOUNDATION_PILE1,
+    FOUNDATION_PILE2,
+    FOUNDATION_PILE3,
+    FOUNDATION_PILE4
+};
+
+static string convert_suits[] = 
+{
+        stringify(CLUBS),
+        stringify(DIAMONDS),
+        stringify(HEARTS),
+        stringify(SPADES)
+};
+
+static string convert_names[] = 
+{
+        stringify(ACE),
+        stringify(TWO),
+        stringify(THREE),
+        stringify(FOUR),
+        stringify(FIVE),
+        stringify(SIX),
+        stringify(SEVEN),
+        stringify(EIGHT),
+        stringify(NINE),
+        stringify(TEN),
+        stringify(JACK),
+        stringify(QUEEN),
+        stringify(KING)
+};
+
+class Card
 {
 public:
-    RectangleShape menu_box, resume, quit, new_game, score_board, start_button, blurred_page;
-    Texture background_texture, tryerz_texture;
-    Sprite background, tryerz_image;
-    Font font;
-    Text timer, start, start_message, made_by, you_won, menu_text, resume_text, new_game_text, quit_text;
-    Clock clock;
-    Time time_elapsed1, time_elapsed2;
-    SoundBuffer window_start_buffer, music_start_buffer, winning_theme_buffer;
-    Sound window_start, music_start, winning_theme_music;
-    OutlinePile* outline_piles;
+    Card();
+    Card(const Card&);
+    Card& operator=(const Card&);
+    friend bool operator==(const Card&, const Card&);
 
-    Game();
-
-    void setMainMenuText();
-    void setMainMenuMusic();
-    void setMainMenuButton();
-    void setBackground();
-    void setTimer();
-    Text drawTimer();
-    void setMenu();
-    void setMenuText();
-    void setOutlinePiles();
-    void drawOutlinePiles(RenderWindow&);
-    void setScoreboard();
-    void setFont();
-    void setEndMenu();
-    void setEndMenuButton();
+    Suits suit;
+    CardNames name;
+    CardColor color;
+    CardPile pile;
+    bool is_flipped;
+    Texture front_img_file, back_img_file;
+    Sprite card_sprite;
+    float x_coordinate, y_coordinate;
 };
