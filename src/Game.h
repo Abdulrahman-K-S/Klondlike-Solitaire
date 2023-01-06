78a619cf3edfd206d4c60d03f13b445d5315bb4c
Ahmad Yasser <ahmadnis123@gmail.com>
2023-01-06 16:18:45 +0200

    Update Game.h

--- src/Game.h
+++ src/Game.h
@@ -7,16 +7,16 @@ public:
     RectangleShape menu_box, resume, quit, new_game, score_board, start_button, blurred_page;
     Texture background_texture, tryerz_texture;
     Sprite background, tryerz_image;
-	Font font;
-	Text timer, start, start_message, made_by, you_won, menu_text, resume_text, new_game_text, quit_text;
+    Font font;
+    Text timer, start, start_message, made_by, you_won, menu_text, resume_text, new_game_text, quit_text;
     Clock clock;
     Time time_elapsed1, time_elapsed2;
     SoundBuffer window_start_buffer, music_start_buffer;
     Sound window_start, music_start;
-    OutlinePile *outline_piles;
+    OutlinePile* outline_piles;
 
     Game();
-    
+
     void setMainMenuText();
     void setMainMenuMusic();
     void setMainMenuButton();
